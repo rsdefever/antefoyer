a56a2493548b801aa95e726e50a67fec8b5a780a
Ryan DeFever <rdefever@nd.edu>
2019-11-04 13:12:05 -0500

    Adding antechamber wrapper and tests

--- antefoyer/antefoyer.py
+++ antefoyer/antefoyer.py
@@ -1,34 +1,199 @@
-"""
-antefoyer.py
-A simple antechamber plugin for foyer
+from __future__ import division
 
-Handles the primary functions
-"""
+import os
+import sys
+import warnings
 
+import parmed as pmd
+import networkx as nx
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+from distutils.spawn import find_executable
+from subprocess import PIPE, Popen
+from foyer.utils.tempdir import temporary_directory
+from foyer.utils.tempdir import temporary_cd
 
-    Replace this function and doc string for your own project
+from foyer.exceptions import FoyerError
+from foyer.utils.io import import_, has_mbuild
+
+ANTECHAMBER = find_executable('antechamber')
+
+def ante_atomtyping(molecule, atype_style):
+    """Perform atomtyping by calling antechamber
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+    molecule : parmed.Structure or mbuild.Compound
+        Molecular structure to perform atomtyping on
+    atype_style : str
+        Style of atomtyping. Options include 'gaff', 'gaff2',
+        'amber', 'bcc', 'sybyl'.
 
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
+    typed_molecule : parmed.Structure
+        The molecule with antechamber atomtyping applied
     """
+    _check_antechamber(ANTECHAMBER)
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+    # Check valid atomtype name
+    supported_atomtypes = ['gaff', 'gaff2', 'amber', 'bcc', 'sybyl' ]
+    if atype_style not in supported_atomtypes:
+        raise FoyerError( 'Unsupported atomtyping style requested. '
+            'Please select from {}'.format(supported_atomtypes))
 
+    # Check for parmed.Structure. Convert from mbuild.Compound if possible
+    molecule = _check_structure(molecule)
+    # Confirm single connected molecule
+    _check_single_molecule(molecule)
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    # Get current directory to write any error logs
+    workdir = os.getcwd()
+    # Work within a temporary directory
+    # to clean up after antechamber
+    with temporary_directory() as tmpdir:
+        with temporary_cd(tmpdir):
+            # Save the existing molecule to file
+            molecule.save('ante_in.mol2')
+            # Call antechamber
+            command = ( 'antechamber -i ante_in.mol2 -fi mol2 '
+                         '-o ante_out.mol2 -fo mol2 ' +
+                         '-at ' + atype_style + ' ' +
+                         '-s 2' )
+
+            proc = Popen(command, stdout=PIPE, stderr=PIPE,
+                         universal_newlines=True, shell=True)
+
+            out, err = proc.communicate()
+
+            # Error handling here
+            if 'Fatal Error' in err or proc.returncode != 0:
+                _antechamber_error(out,err,workdir)
+
+            # Now read in the mol2 file with atomtyping
+            typed_molecule = pmd.load_file('ante_out.mol2',structure=True)
+
+    # And return it
+    return typed_molecule
+
+def ante_charges(molecule, charge_style, net_charge=0.0,
+                           multiplicity=1):
+    """Calculates partial charges by calling antechamber
+
+    Parameters
+    ----------
+    molecule : parmed.Structure or mbuild.Compound
+        Molecular structure to perform atomtyping on
+    charge_style : str
+        Style of partial charges calculation. Options include
+        'bcc', 'gas', and 'mul'. See antechamber documentation
+        by running 'antechamber -L' for details.
+    net_charge : float, optional, default=0.0
+        Net charge of the molecule
+    multiplicity : int, optional, default=1
+        Spin multiplicity, 2S + 1
+
+    Returns
+    -------
+    molecule : parmed.Structure
+        The molecule with charges applied
+    """
+    _check_antechamber(ANTECHAMBER)
+
+    # Check valid atomtype name
+    supported_chargetypes = ['bcc', 'gas', 'mul']
+    if charge_style not in supported_chargetypes:
+        raise FoyerError( 'Unsupported charge style requested. '
+            'Please select from {}'.format(supported_chargetypes))
+
+    # Check for parmed.Structure. Convert from mbuild.Compound if possible
+    molecule = _check_structure(molecule)
+    # Confirm single connected molecule
+    _check_single_molecule(molecule)
+
+    # Get current directory to write any error logs
+    workdir = os.getcwd()
+    # Work within a temporary directory
+    # to clean up after antechamber
+    with temporary_directory() as tmpdir:
+        with temporary_cd(tmpdir):
+            # Save the existing molecule to file
+            molecule.save('ante_in.mol2')
+            # Call antechamber
+            command = ( 'antechamber -i ante_in.mol2 -fi mol2 '
+                         '-o ante_out.mol2 -fo mol2 ' +
+                         '-c ' + charge_style + ' ' +
+                         '-nc ' + str(net_charge) + ' ' +
+                         '-m ' + str(multiplicity) +  ' ' +
+                         '-s 2' )
+
+            proc = Popen(command, stdout=PIPE, stderr=PIPE,
+                         universal_newlines=True, shell=True)
+
+            out, err = proc.communicate()
+
+            # Error handling here
+            if 'Fatal Error' in err or proc.returncode != 0:
+                _antechamber_error(out,err,workdir)
+
+            # Now read in the mol2 file with atomtyping
+            charges = pmd.load_file('ante_out.mol2',structure=True)
+
+    # Combine charge information with existing molecule structure
+    assert len(molecule.atoms) == len(charges.atoms)
+    for atom_idx in range(len(molecule.atoms)):
+        assert molecule.atoms[atom_idx].element == \
+                charges.atoms[atom_idx].element
+        molecule.atoms[atom_idx].charge = charges.atoms[atom_idx].charge
+    return molecule
+
+def _check_structure(molecule):
+    """ Confirm that input is parmed.Structure. Convert
+    from mbuild.Compound to parmed.Structure if possible.
+    """
+    if not isinstance(molecule, pmd.Structure) and has_mbuild:
+        mb = import_('mbuild')
+        if isinstance(molecule, mb.Compound):
+            molecule = molecule.to_parmed()
+
+    if not isinstance(molecule, pmd.Structure):
+        raise FoyerError('Unknown molecule format: {}\n'
+                         'Supported formats are: '
+                         '"parmed.Structure" and '
+                         '"mbuild.Compound"'.format(molecule))
+
+    return molecule
+
+def _check_single_molecule(molecule):
+    """ Confirms that the parmed structure represents a single
+    connect molecule with connectivity info present.
+    """
+    graph_nodes = []
+    graph_edges = []
+    for atom in molecule.atoms:
+        graph_nodes.append(atom.idx)
+    for bond in molecule.bonds:
+        graph_edges.append([bond.atom1.idx,bond.atom2.idx])
+    bond_graph = nx.Graph()
+    bond_graph.add_edges_from(graph_edges)
+    bond_graph.add_nodes_from(graph_nodes)
+    if not nx.is_connected(bond_graph):
+        raise FoyerError("Antechamber requires connectivity information and "
+                         "only supports single molecules (i.e., all atoms "
+                         "in the molecule are connected by bonds.")
+
+def _antechamber_error(out, err, workdir):
+    """Log antechamber output to file. """
+    with open(workdir+'/ante_errorlog.txt', 'w') as log_file:
+        log_file.write("STDOUT:\n\n")
+        log_file.write(out)
+        log_file.write("STDERR:\n\n")
+        log_file.write(err)
+    raise RuntimeError("Antechamber failed. See 'ante_errorlog.txt'")
+
+def _check_antechamber(ANTECHAMBER):
+    if not ANTECHAMBER:
+        msg = ("Antechamber not found. Please ensure that antechamber "
+               "is available. It can be installed via conda, "
+               "'conda install -c conda-forge ambertools'")
+        raise IOError(msg)
+
--- antefoyer/tests/test_antefoyer.py
+++ antefoyer/tests/test_antefoyer.py
@@ -2,11 +2,81 @@
 Unit and regression test for the antefoyer package.
 """
 
-# Import package, test suite, and other packages as needed
-import antefoyer
 import pytest
-import sys
+import parmed as pmd
 
-def test_antefoyer_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "antefoyer" in sys.modules
+from antefoyer.antefoyer import ante_atomtyping
+from antefoyer.antefoyer import ante_charges
+
+from foyer.tests.utils import get_fn
+from foyer.utils.io import has_mbuild
+from foyer.utils.tempdir import temporary_directory
+from foyer.utils.tempdir import temporary_cd
+from foyer.exceptions import FoyerError
+
+
+from distutils.spawn import find_executable
+from os.path import isfile
+
+ANTECHAMBER = find_executable('antechamber')
+
+@pytest.mark.skipif(ANTECHAMBER is not None, reason="antechamber is installed")
+def test_check_antechamber():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    with pytest.raises(IOError):
+        typed = ante_atomtyping(ethane,'gaff')
+
+# TODO: Break this into mult. functions and check atypes
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_valid_atype_style():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    typed = ante_atomtyping(ethane,'gaff')
+    typed = ante_atomtyping(ethane,'gaff2')
+    typed = ante_atomtyping(ethane,'amber')
+    typed = ante_atomtyping(ethane,'bcc')
+    typed = ante_atomtyping(ethane,'sybyl')
+
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_invalid_atype_style():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    with pytest.raises(FoyerError,match=r"Unsupported atomtyping style.*"):
+        typed = ante_atomtyping(ethane,'gaffff')
+
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_ethane_gaff_atypes():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    typed = ante_atomtyping(ethane,'gaff')
+    assert sum((1 for at in typed.atoms if at.type == 'c3')) == 2
+    assert sum((1 for at in typed.atoms if at.type == 'hc')) == 6
+
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_invalid_structure():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    with pytest.raises(FoyerError,match=r"Unknown molecule format.*"):
+        typed = ante_atomtyping(ethane.topology,'gaff')
+
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+@pytest.mark.skipif(not has_mbuild, reason="mbuild is not installed")
+def test_convert_mbuild():
+    import mbuild as mb
+    ethane = mb.load(get_fn('ethane.mol2'))
+    typed = ante_atomtyping(ethane,'gaff')
+
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_multiple_molecules():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    # Remove a bond to break the molecule apart
+    ethane.bonds.remove(ethane.bonds[0])
+    with pytest.raises(FoyerError,match=r"Antechamber requires connectivity.*"):
+        typed = ante_atomtyping(ethane,'gaff')
+
+# TODO: Write this test. What to do about error logfile handling? 
+@pytest.mark.skipif(ANTECHAMBER is None, reason="antechamber is not installed")
+def test_ante_error():
+    ethane = pmd.load_file(get_fn('ethane.mol2'),structure=True)
+    with temporary_directory() as tmpdir:
+        with temporary_cd(tmpdir):
+            with pytest.raises(RuntimeError,match=r"Antechamber failed"):
+                charges = ante_charges(ethane,'bcc',net_charge=-1)
+            assert isfile('ante_errorlog.txt')
+
