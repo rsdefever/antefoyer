b88e406d1aaadc4dc996c299e1217fd48e38160b
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-06-24 21:32:36 -0500

    Add fix to redistribute charges to desired net charge

--- antefoyer/antefoyer.py
+++ antefoyer/antefoyer.py
@@ -15,7 +15,8 @@ from antefoyer.utils.tempdir import temporary_cd
 from foyer.exceptions import FoyerError
 from foyer.utils.io import import_, has_mbuild
 
-ANTECHAMBER = find_executable('antechamber')
+ANTECHAMBER = find_executable("antechamber")
+
 
 def ante_atomtyping(molecule, atype_style):
     """Perform atomtyping by calling antechamber
@@ -36,10 +37,12 @@ def ante_atomtyping(molecule, atype_style):
     _check_antechamber(ANTECHAMBER)
 
     # Check valid atomtype name
-    supported_atomtypes = ['gaff', 'gaff2', 'amber', 'bcc', 'sybyl' ]
+    supported_atomtypes = ["gaff", "gaff2", "amber", "bcc", "sybyl"]
     if atype_style not in supported_atomtypes:
-        raise FoyerError( 'Unsupported atomtyping style requested. '
-            'Please select from {}'.format(supported_atomtypes))
+        raise FoyerError(
+            "Unsupported atomtyping style requested. "
+            "Please select from {}".format(supported_atomtypes)
+        )
 
     # Check for parmed.Structure. Convert from mbuild.Compound if possible
     molecule = _check_structure(molecule)
@@ -53,24 +56,25 @@ def ante_atomtyping(molecule, atype_style):
     with temporary_directory() as tmpdir:
         with temporary_cd(tmpdir):
             # Save the existing molecule to file
-            _write_pdb(molecule,'ante_in.pdb')
+            _write_pdb(molecule, "ante_in.pdb")
             # Call antechamber
-            command = ( 'antechamber -i ante_in.pdb -fi pdb '
-                         '-o ante_out.mol2 -fo mol2 ' +
-                         '-at ' + atype_style + ' ' +
-                         '-s 2' )
+            command = (
+                "antechamber -i ante_in.pdb -fi pdb "
+                "-o ante_out.mol2 -fo mol2 " + "-at " + atype_style + " " + "-s 2"
+            )
 
-            proc = Popen(command, stdout=PIPE, stderr=PIPE,
-                         universal_newlines=True, shell=True)
+            proc = Popen(
+                command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True
+            )
 
             out, err = proc.communicate()
 
             # Error handling here
-            if 'Fatal Error' in err or proc.returncode != 0:
-                _antechamber_error(out,err,workdir)
+            if "Fatal Error" in err or proc.returncode != 0:
+                _antechamber_error(out, err, workdir)
 
             # Now read in the mol2 file with atomtyping
-            typed_molecule = pmd.load_file('ante_out.mol2',structure=True)
+            typed_molecule = pmd.load_file("ante_out.mol2", structure=True)
 
     # Foyer requires that the atom type info is stored under atom.id
     for atom in typed_molecule:
@@ -79,8 +83,10 @@ def ante_atomtyping(molecule, atype_style):
     # And return it
     return typed_molecule
 
-def ante_charges(molecule, charge_style, net_charge=0.0,
-                           multiplicity=1):
+
+def ante_charges(
+    molecule, charge_style, net_charge=0.0, multiplicity=1, charge_tol=0.005
+):
     """Calculates partial charges by calling antechamber
 
     Parameters
@@ -104,10 +110,12 @@ def ante_charges(molecule, charge_style, net_charge=0.0,
     _check_antechamber(ANTECHAMBER)
 
     # Check valid atomtype name
-    supported_chargetypes = ['bcc', 'gas', 'mul']
+    supported_chargetypes = ["bcc", "gas", "mul"]
     if charge_style not in supported_chargetypes:
-        raise FoyerError( 'Unsupported charge style requested. '
-            'Please select from {}'.format(supported_chargetypes))
+        raise FoyerError(
+            "Unsupported charge style requested. "
+            "Please select from {}".format(supported_chargetypes)
+        )
 
     # Check for parmed.Structure. Convert from mbuild.Compound if possible
     molecule = _check_structure(molecule)
@@ -121,46 +129,69 @@ def ante_charges(molecule, charge_style, net_charge=0.0,
     with temporary_directory() as tmpdir:
         with temporary_cd(tmpdir):
             # Save the existing molecule to file
-            _write_pdb(molecule,'ante_in.pdb')
+            _write_pdb(molecule, "ante_in.pdb")
             # Call antechamber
-            command = ( 'antechamber -i ante_in.pdb -fi pdb '
-                         '-o ante_out.mol2 -fo mol2 ' +
-                         '-c ' + charge_style + ' ' +
-                         '-nc ' + str(net_charge) + ' ' +
-                         '-m ' + str(multiplicity) +  ' ' +
-                         '-s 2' )
+            command = (
+                "antechamber -i ante_in.pdb -fi pdb "
+                "-o ante_out.mol2 -fo mol2 "
+                + "-c "
+                + charge_style
+                + " "
+                + "-nc "
+                + str(net_charge)
+                + " "
+                + "-m "
+                + str(multiplicity)
+                + " "
+                + "-s 2"
+            )
 
-            proc = Popen(command, stdout=PIPE, stderr=PIPE,
-                         universal_newlines=True, shell=True)
+            proc = Popen(
+                command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True
+            )
 
             out, err = proc.communicate()
 
             # Error handling here
-            if 'Fatal Error' in err or proc.returncode != 0:
-                _antechamber_error(out,err,workdir)
+            if "Fatal Error" in err or proc.returncode != 0:
+                _antechamber_error(out, err, workdir)
 
             # Now read in the mol2 file with atomtyping
-            charges = pmd.load_file('ante_out.mol2',structure=True)
+            charges = pmd.load_file("ante_out.mol2", structure=True)
+
+            charge_list = [atom.charge for atom in charges]
+            if (net_charge - sum(charge_list)) > charge_tol:
+                raise ValueError(
+                    "The sum of charges defined by antechamber"
+                    " is {}, which differs from the desired net charge"
+                    " of {} by a value greater than {}".format(
+                        sum(charge_list), net_charge, charge_tol
+                    )
+                )
+            elif (net_charge - sum(charge_list)) < charge_tol:
+                charge_delta = (net_charge - sum(charge_list)) / len(charge_list)
+                for atom in charges:
+                    atom.charge += charge_delta
 
     # Combine charge information with existing molecule structure
     assert len(molecule.atoms) == len(charges.atoms)
     for atom_idx in range(len(molecule.atoms)):
-        assert molecule.atoms[atom_idx].element == \
-                charges.atoms[atom_idx].element
+        assert molecule.atoms[atom_idx].element == charges.atoms[atom_idx].element
         molecule.atoms[atom_idx].charge = charges.atoms[atom_idx].charge
     return molecule
 
-def _write_pdb(molecule,filename):
+
+def _write_pdb(molecule, filename):
     """Write a pdb file with CONECT records."""
 
     # Check that we have a box. If not, define one.
     if molecule.box is None:
-        molecule.box = [10.,10.,10.,90.,90.,90.]
+        molecule.box = [10.0, 10.0, 10.0, 90.0, 90.0, 90.0]
 
     assert len(molecule.box) == 6, "Invalid box object."
 
     # Generate CONECT records
-    conect = { atom.idx : [] for atom in molecule.atoms }
+    conect = {atom.idx: [] for atom in molecule.atoms}
     for atom in molecule.atoms:
         atidx = atom.idx
         for bond in molecule.bonds:
@@ -169,40 +200,63 @@ def _write_pdb(molecule,filename):
             elif bond.atom2.idx == atidx:
                 conect[atidx].append(bond.atom1.idx)
 
-    with open(filename, 'w') as pdb:
-        pdb.write('REMARK 1   Created by antefoyer\n')
-        pdb.write('CRYST1{:9.3f}{:9.3f}{:9.3f}{:7.2f}{:7.2f}{:7.2f}'
-                  ' {:9s}{:3d}\n'.format(molecule.box[0],molecule.box[1],
-                      molecule.box[2],molecule.box[3],molecule.box[4],
-                      molecule.box[5],molecule.space_group,1))
+    with open(filename, "w") as pdb:
+        pdb.write("REMARK 1   Created by antefoyer\n")
+        pdb.write(
+            "CRYST1{:9.3f}{:9.3f}{:9.3f}{:7.2f}{:7.2f}{:7.2f}"
+            " {:9s}{:3d}\n".format(
+                molecule.box[0],
+                molecule.box[1],
+                molecule.box[2],
+                molecule.box[3],
+                molecule.box[4],
+                molecule.box[5],
+                molecule.space_group,
+                1,
+            )
+        )
         for atom in molecule.atoms:
-            pdb.write('ATOM  {:5d} {:4s} RES A{:4d}    '
-                    '{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}'
-                    '          {:>2s}  \n'.format(atom.idx+1,atom.name,
-                        0,atom.xx,atom.xy,atom.xz,1.0,0.0,
-                        atom.element_name))
+            pdb.write(
+                "ATOM  {:5d} {:4s} RES A{:4d}    "
+                "{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}"
+                "          {:>2s}  \n".format(
+                    atom.idx + 1,
+                    atom.name,
+                    0,
+                    atom.xx,
+                    atom.xy,
+                    atom.xz,
+                    1.0,
+                    0.0,
+                    atom.element_name,
+                )
+            )
         for atidx, atomlist in conect.items():
-            pdb.write('CONECT{:5d}'.format(atidx+1))
+            pdb.write("CONECT{:5d}".format(atidx + 1))
             for at2idx in atomlist:
-                pdb.write('{:5d}'.format(at2idx+1))
-            pdb.write('\n')
+                pdb.write("{:5d}".format(at2idx + 1))
+            pdb.write("\n")
+
 
 def _check_structure(molecule):
     """ Confirm that input is parmed.Structure. Convert
     from mbuild.Compound to parmed.Structure if possible.
     """
     if not isinstance(molecule, pmd.Structure) and has_mbuild:
-        mb = import_('mbuild')
+        mb = import_("mbuild")
         if isinstance(molecule, mb.Compound):
             molecule = molecule.to_parmed()
 
     if not isinstance(molecule, pmd.Structure):
-        raise FoyerError('Unknown molecule format: {}\n'
-                         'Supported formats are: '
-                         '"parmed.Structure" and '
-                         '"mbuild.Compound"'.format(molecule))
+        raise FoyerError(
+            "Unknown molecule format: {}\n"
+            "Supported formats are: "
+            '"parmed.Structure" and '
+            '"mbuild.Compound"'.format(molecule)
+        )
 
     return molecule
+
 
 def _check_single_molecule(molecule):
     """ Confirms that the parmed structure represents a single
@@ -213,28 +267,33 @@ def _check_single_molecule(molecule):
     for atom in molecule.atoms:
         graph_nodes.append(atom.idx)
     for bond in molecule.bonds:
-        graph_edges.append([bond.atom1.idx,bond.atom2.idx])
+        graph_edges.append([bond.atom1.idx, bond.atom2.idx])
     bond_graph = nx.Graph()
     bond_graph.add_edges_from(graph_edges)
     bond_graph.add_nodes_from(graph_nodes)
     if not nx.is_connected(bond_graph):
-        raise FoyerError("Antechamber requires connectivity information and "
-                         "only supports single molecules (i.e., all atoms "
-                         "in the molecule are connected by bonds.")
+        raise FoyerError(
+            "Antechamber requires connectivity information and "
+            "only supports single molecules (i.e., all atoms "
+            "in the molecule are connected by bonds."
+        )
+
 
 def _antechamber_error(out, err, workdir):
     """Log antechamber output to file. """
-    with open(workdir+'/ante_errorlog.txt', 'w') as log_file:
+    with open(workdir + "/ante_errorlog.txt", "w") as log_file:
         log_file.write("STDOUT:\n\n")
         log_file.write(out)
         log_file.write("STDERR:\n\n")
         log_file.write(err)
     raise RuntimeError("Antechamber failed. See 'ante_errorlog.txt'")
 
+
 def _check_antechamber(ANTECHAMBER):
     if not ANTECHAMBER:
-        msg = ("Antechamber not found. Please ensure that antechamber "
-               "is available. It can be installed via conda, "
-               "'conda install -c conda-forge ambertools'")
+        msg = (
+            "Antechamber not found. Please ensure that antechamber "
+            "is available. It can be installed via conda, "
+            "'conda install -c conda-forge ambertools'"
+        )
         raise IOError(msg)
-
